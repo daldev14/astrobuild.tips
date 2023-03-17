c162ead69700a364c853bc252834de403f2605b7
Daniel Amaya <damayalop@gmail.com>
2023-03-17 11:19:36 -0600

    [UPDATE] Components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,11 @@ const Header = ({title}: Props) => {
   return (
     <div className="mx-auto max-w-screen-xl py-8">
       <h1 className="mb-4">
-        <span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-purple-600">
-          {title}
-        </span>
+        <a href="/">
+          <span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-purple-600">
+            {title}
+          </span>
+        </a>
       </h1>
     </div>
   );
